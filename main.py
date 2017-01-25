2397311947d347e26ef1336ffabdd3895eb254ed
Cesar Luis Jaitman Labaton <cesarjaitmanlabaton@gmail.com>
2017-01-25 10:37:37 -0500

    added login and welcome functionality

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 # Importing different libraries
 import os
+import re
+import hashlib
+import random
+import hmac
 import webapp2
 import jinja2
+
+from string import letters
+from google.appengine.ext import db
 
 
 # Directing where the templates will be
@@ -10,6 +17,74 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 # Setting up jinja2
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
+
+# Hashed secret for extra layer of security for cookies
+SECRET = "1234567890"
+
+
+# Creating function to make values secured
+def make_secure_val(val):
+    return "%s|%s" % (val, hmac.new(SECRET, val).hexdigest())
+
+
+# Creating function to make sure the values are secured
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+
+# Creating function that makes randomiser of letters
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+# Creating class that render jinja and short hands some responses
+class Handler(webapp2.RequestHandler):
+
+    # Shortens the command to .write
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    # Render jinja and params for user
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    # Setting up and securing cookies
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add.headers(
+            'Set-Cookie', '%s=%s; Path=/' % (name, cookie_val))
+
+    # Opening and reading the secured cookie
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    # Sets a secure cookie to the login
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    # Sets a secure cookie to the logout
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+
+# MainPage class
+class MainPage(Handler):
+
+    def get(self):
+        self.write("Hello World!")
 
 
 # Verifies for correct characters in the username
@@ -36,47 +111,169 @@ def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
 
-# Creating class that render jinja and short hands some responses
-class Handler(webapp2.RequestHandler):
-
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
-
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
-
-# MainPage class
-class MainPage(Handler):
-
-    def get(self):
-        self.write("Hello World!")
-
-
 # Class for the sign up sheet where the user inputs their information
 class SignUpHandler(Handler):
 
+    # Get of the html file signup
     def get(self):
         self.render('signup.html')
 
+    # Posting of information and creation of different variables
     def post(self):
-        error = False
+        have_error = False
         username = self.request.get('username')
         password = self.request.get('password')
+        verifypassword = self.request.get('verifypassword')
         email = self.request.get('email')
 
-    params = dict(username=username, email=email)
+        # Creation of parameters in case there is an error and everything does
+        # not get erased
+        params = dict(username=username, email=email)
 
-    if not valid_username(username):
-        params['error_message'] = "That's not a valid username."
-        error = True
+        # Checks if the username is valid with the fuctions defined before
+        if not valid_username(username):
+            params['error_message'] = "That's not a valid username."
+            have_error = True
+
+        # Checks if the password is valid with the fuctions defined before
+        if not valid_password(password):
+            params['error_password'] = "That's not a valid password."
+            have_error = True
+        elif password != verifypassword:
+            params['error_verifypassword'] = "Passwords do NOT match!"
+            have_error = True
+
+        # Checks if the email is valid with the fuctions defined before
+        if not valid_email(email):
+            params['error_email'] = "That't not a valid e-mail."
+            have_error = True
+
+        # If there is no error, render welcome page othewise show errors
+        if have_error:
+            self.render('/signup.html', **params)
+        else:
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
+
+
+# Creates a random letters in order to strengthen the hash
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+# Creates the hash and incorporates the salt
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (salt, h)
+
+
+def valid_pw(name, password, h):
+    salt = h.split(',')[0]
+    return h == make_pw_hash(name, password, salt)
+
+
+def users_key(group='default'):
+    return db.Key.from_path('users', group)
+
+
+# Creating class for the user
+class User(db.Model):
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+
+    # Decorator
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+
+# Class that redirects the user after the successfully completed signup page
+class Signup(SignUpHandler):
+
+    def done(self):
+        self.redirect('/welcomepage?username=', self.username)
+
+
+# Register class handler
+class Register(SignUpHandler):
+
+    def done(self):
+        userLogin = User.by_name(self.username)
+        if userLogin:
+            message = "That user already exists!"
+            self.render('singup.html', error_username=message)
+        else:
+            userLogin = User.register(self.username, self.password, self.email)
+            userLogin.put()
+
+            self.login(userLogin)
+            self.redirect('/welcome')
+
+
+class WelcomePageHandler(SignUpHandler):
+
+    def get(self):
+        if self.user:
+            self.render('welcome.html', username=self.user.name)
+        else:
+            self.redirect('/signup')
+
+
+# Login class handler
+class LoginHandler(SignUpHandler):
+
+    def get(self):
+        self.render('login.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+
+        userLogin = User.login(usename, password)
+        if userLogin:
+            self.login(userLogin)
+            self.redirect('/blog.html')
+        else:
+            message = "Invalid login!"
+            self.render('login.html', error=message)
+
+
+# Logout class handler
+class LogoutHandler(SignUpHandler):
+
+    def get(self):
+        self.logout()
+        self.redirect('/blog.html')
 
 
 # List of different webpages and connected to their respective classes.
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/signup', SignUpHandler),
-                               ], debug=True)
+                               ('/login', LoginHandler),
+                               ('/logout', LogoutHandler),
+                               ('/welcomepage', WelcomePageHandler)],
+                              debug=True)
